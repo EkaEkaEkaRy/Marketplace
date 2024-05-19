d3161ce412c0fa72d6c02b6723d56c15fbd7d493
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-05-19 19:27:19 +0300

    Update select_flower.jsx

--- Фронтенд/my-react-app/src/seller/create_sklad/create_flower/components/select_flower.jsx
+++ Фронтенд/my-react-app/src/seller/create_sklad/create_flower/components/select_flower.jsx
@@ -2,20 +2,28 @@ import React, { useState, useRef, useEffect } from 'react';
 import s from './select_flower.module.css'
 
 const ProductSelector = () => {
+
+  async function  getListOfTypes(){
+      let type_flowers = [];
+      const res = await fetch('http://localhost:1337/api/flower-type', {
+          method: "GET",
+          headers: { "Accept": "application/json", "Content-Type":
+          "application/json" }
+      });
+      if (res.status === 404) console.log('Цветы не найдены');
+      else if (res.ok) {
+            const data = await res.json();
+            data.forEach((i) => {
+              type_flowers.push(i['name'])
+            })
+            localStorage.setItem('ListOfTypes', type_flowers)
+      }
+    }
+
+  getListOfTypes();
+
   const [inputValue, setInputValue] = useState('');
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const products = [
-    'Product A',
-    'Product B',
-    'Product C',
-    'Product D',
-    'Product E',
-    'Product F',
-    'Product G',
-    'Product H',
-    'Product I',
-    'Product J',
-  ];
+  const products = localStorage.getItem('ListOfTypes').split(",");
 
   const handleInputChange = (event) => {
     const value = event.target.value;
@@ -24,7 +32,7 @@ const ProductSelector = () => {
 
   const getMatchingProducts = () => {
     const regex = new RegExp(inputValue, 'i');
-    const matchingProducts = products.filter((product) => regex.test(product) && !selectedProducts.includes(product)).slice(0, 3);
+    const matchingProducts = products.filter((product) => regex.test(product)).slice(0, 3);
     return matchingProducts[0] === inputValue && matchingProducts.length === 1? null: matchingProducts.length > 0 ? matchingProducts : ['Цветок не найден'];
   };
 
